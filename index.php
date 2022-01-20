f3fdbb840e62cda96bd15628e9586dc71e790694
Aaron KASONGO <19kk181@esisalama.org>
2022-01-20 21:48:46 +0100

    Ajout d'un formulaire sur la page index.php

--- index.php
+++ index.php
@@ -1,3 +1,52 @@
-<?php
-echo 'Hello world, Hello ESIS'
- ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="get">
+        <div>
+            Nom: 
+            <input type="text" name="name">
+        </div>
+        <br>
+        <div>
+            PostNom: 
+            <input type="text" name="second_name">
+        </div>
+        <br>
+        <div>
+            Prenom: 
+            <input type="text" name="prenom">
+        </div>
+        <br>
+        <div>
+            Matricule: 
+            <input type="text" name="mat">
+        </div>
+        <br>
+        Genre: 
+        <select name="genre" id="genre">
+            <option value="M">Masculin</option>
+            <option value="F">Feminin</option>
+        </select>
+        <br>
+        <div>
+            Promotion: 
+            <input type="text" name="promotion">
+        </div>
+        <br>
+        <div>
+            Email
+            <input type="email" name="email">
+        </div>
+        <button type="submit">OK</button>
+    </form>
+</body>
+
+</html>
